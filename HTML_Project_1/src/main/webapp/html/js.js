37d6c584faaba4d2d1977437022259f20363aa0f
john1015 <10151404john@gmail.com>
2024-06-24 16:49:55 +0900

    0624

---
+++ HTML_Project_1/src/main/webapp/html/js.js
@@ -0,0 +1,11 @@
+function start1 () {
+		alert("자바 스크립트 시작");
+	}
+	let start2  = function(){
+		alert("자바 스크립트 시작2")
+	}
+	// 자바 스크립트 6버전에서 주로 사용 => Arrow 함수 => function , return을 제거해서 사용
+	// 자바 => 람다식
+	let start3 =()=>{
+		alert("자바 스크립트 시작3")
+	}
