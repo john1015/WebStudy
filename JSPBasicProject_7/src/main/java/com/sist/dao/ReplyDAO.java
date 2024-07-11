02d582dca3b7054b728a6081e7ab652fde0d8e64
john1015 <10151404john@gmail.com>
2024-07-11 15:20:59 +0900

    0710(리스트출력)-0711(상세보기/댓글)

--- JSPBasicProject_7/src/main/java/com/sist/dao/ReplyDAO.java
+++ JSPBasicProject_7/src/main/java/com/sist/dao/ReplyDAO.java
@@ -1,5 +1,111 @@
 package com.sist.dao;
 
-public class ReplyDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.*;
+import com.sist.database.DataBaseConnection;
 
+public class ReplyDAO {
+	private Connection conn;
+	private PreparedStatement ps;
+	private static ReplyDAO dao;
+	private DataBaseConnection dbConn = new DataBaseConnection();
+	
+	public static ReplyDAO newInstance() {
+		if(dao==null) dao = new ReplyDAO();
+		return dao;
+	}
+	
+	/*
+	try {
+		conn = dbConn.getConnection();
+		String sql = "select ";
+	} catch (Exception ex) {
+		ex.printStackTrace();
+	} finally {
+		dbConn.disConnection(conn, ps);
+	}
+	*/
+	
+	// 1. 댓글 추가
+	public void replyInsert(ReplyVO vo) {
+		try {
+			conn = dbConn.getConnection();
+			String sql = "insert into food_reply values( "
+					+ "	  fr_rno_seq.nextval , ? , ? , ? , ? , SYSDATE) ";
+			ps=conn.prepareStatement(sql);
+			ps.setInt(1, vo.getFno());
+			ps.setString(2, vo.getId());
+			ps.setString(3, vo.getName());
+			ps.setString(4, vo.getMsg());
+			
+			ps.executeUpdate();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			dbConn.disConnection(conn, ps);
+		}
+	}
+	
+	// 2. 데이터 읽기
+	public List<ReplyVO> replyListData(int fno){
+		List<ReplyVO> list = new ArrayList<ReplyVO>();
+		try {
+			conn = dbConn.getConnection();
+			String sql = "select /*+ INDEX_DESC(food_reply fr_rno_pk)*/rno,fno,id,name,msg,TO_CHAR(regdate,'yyyy-mm-dd HH24:MI:SS') "
+					+ "	   from food_reply "
+					+ "	   where fno= "+fno;
+			ps=conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()) {
+				ReplyVO vo = new ReplyVO();
+				vo.setRno(rs.getInt(1));
+				vo.setFno(rs.getInt(2));
+				vo.setId(rs.getString(3));
+				vo.setName(rs.getString(4));
+				vo.setMsg(rs.getString(5));
+				vo.setDbday(rs.getString(6));
+				list.add(vo);
+				
+			}
+			rs.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			dbConn.disConnection(conn, ps);
+		}
+		return list;
+	}
+	// 댓글 삭제
+	public void replyDelete(int rno) {
+		try {
+			conn = dbConn.getConnection();
+			String sql = "delete from food_reply where rno="+rno;
+			ps=conn.prepareStatement(sql);
+			ps.executeUpdate();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			dbConn.disConnection(conn, ps);
+		}
+	}
+	
+	public void replyUpdate(ReplyVO vo) {
+		try {
+			conn = dbConn.getConnection();
+			String sql = "update food_reply set "
+					+ "	   msg=? "
+					+ " 	   where rno=?";
+			ps=conn.prepareStatement(sql);
+			ps.setString(1, vo.getMsg());
+			ps.setInt(2,vo.getRno());
+			
+			ps.executeUpdate();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			dbConn.disConnection(conn, ps);
+		}
+	}
 }
