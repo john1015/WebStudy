daa7e6c3c8a037c7d4c93e77cc2672222c70d56c
john1015 <10151404john@gmail.com>
2024-07-16 17:04:43 +0900

    0716(일정관리-달력/메모)

---
+++ JSPBasicProject_12/src/main/java/com/sist/dao/DiaryVO.java
@@ -0,0 +1,23 @@
+package com.sist.dao;
+
+import java.util.Date;
+
+import lombok.Data;
+
+/*
+ * 
+ *  NO                                        NOT NULL NUMBER
+ ID                                                 VARCHAR2(20)
+ SUBJECT                                   NOT NULL VARCHAR2(1000)
+ MSG                                       NOT NULL CLOB
+ YEAR                                               NUMBER(4)
+ MONTH                                              NUMBER(2)
+ DAY                                                NUMBER(2)
+ REGDATE                                            DATE
+ */
+@Data
+public class DiaryVO {
+	private int no , year , month , day;
+	private String subject , id , msg ;
+	private Date regDate;
+}
