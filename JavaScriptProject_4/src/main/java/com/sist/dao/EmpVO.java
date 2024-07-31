e5cad75a8364d78caf65d9b494914bc6b5101d6e
john1015 <10151404john@gmail.com>
2024-07-31 17:35:15 +0900

    0731

---
+++ JavaScriptProject_4/src/main/java/com/sist/dao/EmpVO.java
@@ -0,0 +1,12 @@
+package com.sist.dao;
+
+import java.util.Date;
+
+import lombok.Data;
+// => MyBatis : 컬럼명과 일치 => 변경
+@Data
+public class EmpVO {
+	private int empno,sal,comm,deptno,mgr;
+	private String ename ,job;
+	private Date hiredate;
+}
